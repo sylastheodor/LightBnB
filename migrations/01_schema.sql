7ce513166d411ff26ef434c73c19d47efccd5a2b
Sylas Serné <its.sylas@gmail.com>
2021-07-22 22:41:23 +0000

    structure half of the LighBnB database"

---
+++ migrations/01_schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255),
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night DECIMAL,
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(7),
+  active BOOLEAN 
+)
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+)
